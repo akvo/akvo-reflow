65608970b920e0969bb4d9a30b5c99dac547f917
Iván Perdomo <ivan@akvo.org>
2016-07-05 07:29:41 +0200

    [#6] Adds reload endpoint

---
+++ src/akvo_reflow/endpoint/reload.clj
@@ -0,0 +1,12 @@
+(ns akvo-reflow.endpoint.reload
+  (:require [akvo-reflow.main :as main]
+            [clojure.java.shell :as shell]
+            [compojure.core :refer [routes POST]]))
+
+(defn reload-endpoint [config]
+  (routes
+   (POST "/" []
+     (let [pull (shell/with-sh-dir (:flow-server-config main/config) ;; FIXME
+                  (shell/sh "git" "pull"))]
+       (when (zero? (:exit pull))
+         (reload-flow-config))))))
