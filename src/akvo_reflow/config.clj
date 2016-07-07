93f8f5653b8a25b76e09ebdee7870b8ad3d6061b
Iván Perdomo <ivan@akvo.org>
2016-07-07 10:03:30 +0200

    [#6] Code improvments from code review (via @jonase)

--- src/akvo_reflow/config.clj
+++ src/akvo_reflow/config.clj
@@ -1,8 +1,7 @@
 (ns akvo-reflow.config
   (:require [akvo.commons.config :refer [get-config]]
             [environ.core :refer [env]]
-            [me.raynes.fs :refer [find-files exists?]])
-  (:import java.io.File))
+            [me.raynes.fs :refer [find-files exists?]]))
 
 (def defaults
   ^:displace {:http {:port 3000}})
@@ -24,8 +23,7 @@
 (defn get-flow-config
   "Returns a map {\"app-id\" {config}}"
   [flow-server-config]
-  {:pre [(not (nil? flow-server-config))
-         (not-empty flow-server-config)
+  {:pre [(not-empty flow-server-config)
          (exists? flow-server-config)]}
   (->> #"appengine-web.xml"
        (find-files flow-server-config)
