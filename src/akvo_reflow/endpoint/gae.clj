0b077894b91e8688844e4fe45b4388ce3ae4d012
Gabriel von Heijne <gabriel@akvo.org>
2016-08-01 12:00:08 +0200

    [#16] Call process-events when GAE posts
    Add call using future to process-events in the gae endpoint.

--- src/akvo_reflow/endpoint/gae.clj
+++ src/akvo_reflow/endpoint/gae.clj
@@ -1,9 +1,15 @@
 (ns akvo-reflow.endpoint.gae
-  (:require [akvo-reflow.utils :refer [with-db-schema]]
+  (:require [akvo-reflow.unilogger :refer [process-events]]
+            [akvo-reflow.utils :refer [with-db-schema]]
             [compojure.core :refer [context POST]]
             [hugsql.core :as hugsql]))
 
 (hugsql/def-db-fns "akvo_reflow/endpoint/gae.sql")
+
+(defn do-process-events
+  "wrapper around the future to be able swap it out for testing"
+  [conn org-id]
+  (future (process-events conn org-id)))
 
 (defn endpoint [{:keys [db]}]
   (context "/gae" []
@@ -13,7 +19,8 @@
           (let [ds (select-keys (-> db :spec) [:datasource])]
             (with-db-schema [conn ds] org-id
               (doseq [event (get body "events")]
-                (insert-event conn {:payload event})))
+                (insert-event conn {:payload event}))
+              (do-process-events conn org-id))
             {:status 200
              :body {:status "OK"}})
           {:status 400
