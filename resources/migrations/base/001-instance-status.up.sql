aedc4073a8dff0e7ca3f2783a6c6d6c2ebf9ce3a
Gabriel von Heijne <gabriel@akvo.org>
2016-08-05 11:42:03 +0200

    [#18] Fix index creating SQL
    Seems IF NOT EXISTS isn't supported for PG 9.4- when creating an index.

--- resources/migrations/base/001-instance-status.up.sql
+++ resources/migrations/base/001-instance-status.up.sql
@@ -5,5 +5,5 @@ CREATE TABLE IF NOT EXISTS instance_status
   import_done boolean DEFAULT FALSE,
   export_done boolean DEFAULT FALSE);
 
-CREATE UNIQUE INDEX IF NOT EXISTS instance_id_unique_index
+CREATE UNIQUE INDEX instance_id_unique_index
   ON instance_status (instance_id);
